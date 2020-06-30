c8557409a0de0345531ed2df11bad67db13ebd8c
Daniel Bengtsson <daniel.bengtsson@live.com>
2020-06-30 22:17:21 +0200

    Migrated to VS 2019 and .NET Standard 2.1

--- Extensions/DSharpExtensions.cs
+++ Extensions/DSharpExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
